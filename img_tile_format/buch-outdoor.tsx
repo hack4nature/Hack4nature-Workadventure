d0287400eb8a0b21049d3eca65d5fd42c4d793c4
Hervé Aymes <herve_aymes@natural-solutions.eu>
2021-03-26 12:33:54 +0100

    outdoor

---
+++ img_tile_format/buch-outdoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="buch-outdoor" tilewidth="32" tileheight="32" tilecount="72" columns="12">
+ <image source="C:/Program Files/Tiled/examples/buch-outdoor.png" width="384" height="192"/>
+</tileset>

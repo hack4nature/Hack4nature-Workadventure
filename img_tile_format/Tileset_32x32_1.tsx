268e4cb6e42a0d7fcc1e177f12a3f4974b472e44
Jeje2201 <jeremy_leriche@natural-solutions.eu>
2021-03-26 13:49:05 +0100

    Jeremy push

---
+++ img_tile_format/Tileset_32x32_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Tileset_32x32_1" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../Modern tiles_Free/Interiors_free/32x32/Tileset_32x32_1.png" width="320" height="320"/>
+</tileset>
